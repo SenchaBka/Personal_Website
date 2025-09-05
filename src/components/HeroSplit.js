eb628bbc79f99f177132762b7e1f5be776351a3d
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2025-09-05 18:38:24 -0400

    Change of background photos

--- src/components/HeroSplit.js
+++ src/components/HeroSplit.js
@@ -1,17 +1,17 @@
 import { motion } from "framer-motion";
 import { useState} from "react";
 
-import bgPhotographer from "../assets/photos/photographer_bg.jpg"
+import bgPhotographer from "../assets/photos/filmmaking_bg.jpg"
 import bgSoftware from "../assets/photos/software_bg.jpg"
 
 export default function HeroSplit(){
   const [hovered, setHovered] = useState(null);
 
   return (
-    <div className="flex h-screen w-full overflow-hidden relative">
+    <div className="flex h-screen w-full relative">
       {/*Left Side*/}
       <motion.div
-        className="flex-1 flex items-center justify-center relative z-10 overflow-hidden"
+        className="flex-1 flex items-center justify-center relative"
         onMouseEnter={() => setHovered("left")}
         onMouseLeave={() => setHovered(null)}
         animate={{flex: hovered === "left" ? 3 : hovered === "right" ? 1 : 1}}
@@ -26,23 +26,23 @@ export default function HeroSplit(){
         />
         {/*Text*/}
         <motion.div
-          className="z-10 backdrop-blur-md bg-black/30 p-4 rounded"
+          className="backdrop-blur-md bg-black/30 p-4 rounded"
           animate={{
             opacity: hovered === "left" ? 1 : 0.7,
             scale: hovered === "left" ? 1.1 : 1,
           }}
           transition={{ duration: 0.5 }}
         >
-          <h1 className="text-4xl font-bold text-white">Photography</h1>
+          <h1 className="text-4xl font-bold text-white">Filmmaking</h1>
         </motion.div>
       </motion.div>
 
       {/*Divider*/}
-      <motion.div className="w-[2px] bg-white z-10" />
+      <motion.div className="w-[2px] bg-white" />
 
       {/*Right Side*/}
       <motion.div
-        className="flex-1 flex items-center justify-center relative z-10 overflow-hidden"
+        className="flex-1 flex items-center justify-center relative"
         onMouseEnter={() => setHovered("right")}
         onMouseLeave={() => setHovered(null)}
         animate={{flex: hovered === "right" ? 3 : hovered === "left" ? 1 : 1}}
@@ -57,14 +57,14 @@ export default function HeroSplit(){
         />
         {/*Text*/}
         <motion.div
-          className="z-10 backdrop-blur-md bg-black/30 p-4 rounded"
+          className="backdrop-blur-md bg-black/30 p-4 rounded"
           animate={{
             opacity: hovered === "right" ? 1 : 0.7,
             scale: hovered === "right" ? 1.1 : 1,
           }}
           transition={{ duration: 0.5 }}
         >
-          <h1 className="text-4xl font-bold text-white">Software</h1>
+          <h1 className="text-4xl font-bold text-white">Software Engineering</h1>
         </motion.div>
       </motion.div>
     </div>
